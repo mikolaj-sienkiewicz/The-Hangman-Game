7058fb5ab1f14a68ce92df0ccecd977ead48390c
Adam Stelmaszyk <adamstelm98@gmail.com>
2020-01-15 00:36:30 +0100

    Next commit

--- newSerwerProgram.cpp
+++ newSerwerProgram.cpp
@@ -67,7 +67,7 @@ int main(int argc, char **argv)
                 }
                 else
                 {
-                    getMessageFromUser(i)
+                    getMessageFromUser(i);
                 }
                 ready--;
             }
